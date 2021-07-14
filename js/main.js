b8ee0c27a57e8b11a1d3f10c31e1c50a8d7cb1b4
sharkova.ev <sharkova.ev@phystech.edu>
2021-07-14 22:17:31 +0300

    добавлен слайдер

--- js/main.js
+++ js/main.js
@@ -1,4 +1,9 @@
 $(function(){
-    
+    $('.fairy-tail__slider').slick({
+        fade: true, //сделали слайдер более плавным
+        prevArrow: '<button type="button" class=" slick-btn slick-prev"><img src="images/arrow-left.svg" alt="стрелка влево"></button>',
+        nextArrow: '<button type="button" class=" slick-btn slick-next"><img src="images/arrow-right.svg" alt="стрелка вправо"></button>',
+        autoplay: true,
+    }); //сделали слайдер
    
 });
