e61f0a434bc918161ce1ff6dab0fbef8a2085a08
sharkova.ev <sharkova.ev@phystech.edu>
2021-07-15 12:25:42 +0300

    Добавлена вторая секция главной страницы сайта

--- js/main.js
+++ js/main.js
@@ -5,5 +5,11 @@ $(function(){
         nextArrow: '<button type="button" class=" slick-btn slick-next"><img src="images/arrow-right.svg" alt="стрелка вправо"></button>',
         autoplay: true,
     }); //сделали слайдер
-   
+    
+    $('.our-trip__slider').slick({
+        fade: true, //сделали слайдер более плавным
+        prevArrow: '<button type="button" class=" slick-btn slick-prev"><img src="images/arrow-left.svg" alt="стрелка влево"></button>',
+        nextArrow: '<button type="button" class=" slick-btn slick-next"><img src="images/arrow-right.svg" alt="стрелка вправо"></button>',
+        autoplay: true,
+    }); //сделали слайдер
 });
